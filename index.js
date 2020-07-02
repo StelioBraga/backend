ca75760ae2402689b914d030446c3d0c90f6147f
steliobraga <Steliobraga13@gmail.com>
2020-07-02 22:42:54 +0200

    remove

--- index.js
+++ index.js
@@ -4,20 +4,24 @@ const { Pool, Client } = require('pg');
 const sendgridMail = require('@sendgrid/mail');
 const xl = require('excel4node');
 const env = require('dotenv')
+const Mailgun = require('mailgun-js');
+
 
 env.config();
 sendgridMail.setApiKey(process.env.SENDGRID_API_KEY);
+var mailgun = new Mailgun({apiKey:process.env.MAILGUN_API_KEY, domain:process.env.MAILGUN_DOMAIN});
 
+//send email
 function sendEmail( name, email) {
-   
 // msg container
-const msg = {
+const emailContainer = {
     to:'steliobraga13@gmail.com',
-    from:'desk2U@2ibi.com',
+    from:'kcruz@paytech.tech',
     subject:'new user created',
-    text:'The user '+ name+'wiht'+email+' ' ,
+    text:'The user '+ name +' wiht email:'+ email +' has been join.' ,
 };
-sendgridMail.send(msg);
+//sendgridMail.send(emailContainer);
+mailgun.messages().send(emailContainer)
 }
 
 /**
@@ -26,7 +30,7 @@ sendgridMail.send(msg);
  */
 const createSheet = (rows, res) => {
     const workbook = new xl.Workbook();
-    const sheet = workbook.addWorksheet('Usuários')
+    const sheet = workbook.addWorksheet('Users')
     // add data to the sheet
     // 1. Header
     sheet.cell(1,1).string('Nome')
@@ -42,7 +46,7 @@ const createSheet = (rows, res) => {
     });
 
     // And finally.. write it into the filesystem.
-    workbook.write("excelnovo.xlsx", res);
+    workbook.write("Users.xlsx", res);
 }
 
 // pools will use environment variables
@@ -55,17 +59,17 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 
 
-// send the mail
 app.post('/api/user', function(req, res) {
    // console.log(req.body);
+   
     const name = req.body.name;
     const email = req.body.email;
     pool.query("INSERT INTO users values($1, $2)", [name,email], (error, result) => {
         if(error){
            console.log(error);
         }else{
-            console.log("Terminou de executar INSERT")
-          //  sendEmail(name,email)
+            console.log("User created successfully")
+           sendEmail(name,email)
            res.sendStatus(200).end;
         }
     })
